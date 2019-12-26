0b5d41a4392f98e8b8268f08b9c606ad31578ea3
aggfrassica <aggfrassica@gmail.com>
2019-12-26 20:24:19 +0100

    Added navbars (dynamic load with php) on PC pages

---
+++ Assembler/PC/pc6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>pc6</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./../css/navbar.css">
+        <link rel="stylesheet" href="./css/scroll.css">
+        <link rel="stylesheet" href="./slick-1.8.1/slick/slick.css">
+        <link rel="stylesheet" href="./slick-1.8.1/slick/slick-theme.css">
+        <link rel="stylesheet" href="./css/textwriter.css">
+        <link rel="stylesheet" href="./css/sliderimage.css">
+        <link rel="stylesheet" href="./css/hvr_image.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+    </head>
+    
+    <body>
+       
+        <?php 
+        
+        include(__DIR__ .'/../php_Assembler/navbar.php');
+        
+        
+        ?>
+    </body>
+    
+    
+    </html>
---
+++ Assembler/php_Assembler/navbar.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '<header>
+<img class="logo" src="images/logo.svg" alt="logo">
+<nav>
+    <ul class="nav__links">
+        <li class="vetrina"><a href="./../index.html" >Home</a></li>
+        <li class="assembla"><a href="#" >Assembla</a></li>
+       
+   </ul>
+</nav>
+<a class="cta" href="#">?</a>
+
+</header>';
+
+?>
