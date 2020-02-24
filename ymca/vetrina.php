75b834a8d72e99bc40c343ea41b7daad7a2b97dd
unknown <aggfrassica@gmail.com>
2020-02-24 15:40:01 +0100

    Grid Fixed

--- ymca/vetrina.php
+++ ymca/vetrina.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 	<title>PC Home</title>
 </head>
 <body>
@@ -36,18 +36,19 @@
 	<?php
 		if (isset($_POST["Fascia"])){
 			$fascia=strval($_POST["Fascia"]);
-			echo $fascia;
+			
 			$query1="SELECT *from pc where Fascia = '$fascia'";
-
+			
 			$data=$connection->query($query1);
 			foreach($data as $row){ 
 
-				echo '<div class="pc1">
+				echo '<div class="pc">
 				<div class="circle">
 					<div class="prova"></div>
 					<img src="data:image/png;base64,'.base64_encode($row["img"]).'" alt="">
 				</div>
 			</div>';
+			
 		}
 
 		}
@@ -57,14 +58,13 @@
 			
 	
 		
-			$query="SELECT * FROM `PC`
-			ORDER BY Prezzo ASC ";
+			$query="SELECT * FROM `PC` ORDER BY Prezzo ASC";
 				$data=$connection->query($query);
 				foreach($data as $row){ 
 
 						echo '
 					
-						<div class="pc1">
+						<div class="pc">
 				<div class="circle">
 					<div class="prova"></div>
 					<img src="data:image/png;base64,'.base64_encode($row["img"]).'" alt="">
